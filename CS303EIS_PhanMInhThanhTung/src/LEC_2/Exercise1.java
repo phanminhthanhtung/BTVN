9bb2dbef8be6e0d60ea9cd15096bcef66182cfcc
Thanh Tùng <tungphanminhthanh@gmail.com>
2025-08-13 19:48:09 +0700

    First Commit

---
+++ CS303EIS_PhanMInhThanhTung/src/LEC_2/Exercise1.java
@@ -0,0 +1,21 @@
+package cs303eis_phanminhthanhtung;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        int  x;
+        x = 1 + 2;
+        System.out.println("(1): "+ x);
+        x = (int)(3.5 + 8);
+        System.out.println("(2): "+ x);
+        x = (1 / 2);
+        System.out.println("(3): "+ x);
+        x = (int)(4.5 + 7);
+        System.out.println("(4): "+ x);
+        x = 4 * 5 + 1;
+        System.out.println("(5): "+ x);
+        x = 20 % 3 + 2 - 7;
+        System.out.println("(6): "+ x);
+    }
+    
+}
---
+++ CS303EIS_PhanMInhThanhTung/src/LEC_2/Exercise21and22.java
@@ -0,0 +1,20 @@
+package cs303eis_phanminhthanhtung;
+
+public class Exercise21and22 {
+    
+    public static void main(String[] args) {
+        //Exercise 21
+        int x1 = 1;
+        int y1 = x1 + ++x1 - x1-- - x1++ + x1 - x1--;
+        System.out.println("x = " + x1);
+        System.out.println("y = " + y1);
+        
+        System.out.println("");
+        
+        //Exercise 22
+        int x2 = 1;
+        int y2 = x2 - x2-- - x2++ + x2-- - x2 + x2-- + x2;
+        System.out.println("x = " + x2);
+        System.out.println("y = " + y2);
+    }
+}
