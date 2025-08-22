e865364aa89da74c4ad60b496db98d6c2580b4be
David Phan <tungphanminhthanh@gmail.com>
2025-08-22 08:58:04 +0700

    Add files via upload

--- CS303EIS_PhanMInhThanhTung/src/LEC_5/Exercise11.java
+++ CS303EIS_PhanMInhThanhTung/src/LEC_5/Exercise11.java
@@ -5,16 +5,14 @@ public class Exercise11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random rand = new Random();
+        int index = 1, tries = 0;
+        int num1 = 1, num2 = 0;
         do{    
-            int num1 = rand.nextInt(6) + 1;
-            int num2 = rand.nextInt(6) + 1;
-            int index = 1, tries = 0;
+            num1 = rand.nextInt(6) + 1;
+            num2 = rand.nextInt(6) + 1;
             System.out.println("Roll " + (index++) + ": " + num1 + " and " + num2);
             tries++;
-            if (num1 == num2){
-                System.out.println("You rolled doubles after " + tries + " tries!   ");
-                break; 
-            }
-        } while(true);
+        } while(num1 != num2);      
+        System.out.println("You rolled doubles after " + tries + " tries!   ");
     }
 }
